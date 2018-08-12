87730f41b3fd6c0977de262f7b0e4146323aecf9
Jonas Isberg <jonas.d.isberg@gmail.com>
2018-08-12 10:21:44 +0200

    feat: Process -> MainWindow:AutomationElement

---
+++ UIAutomationClient.FluentExtensions.Tests/ProcessExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace UIAutomationClient.FluentExtensions.Tests
+{
+    public class ProcessExtensionsTests
+    {
+        private Process sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            sut = Process.Start("notepad");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            try
+            {
+                sut.WaitForInputIdle(200);
+                sut.CloseMainWindow();
+                sut.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception in Cleanup(): {ex.Message}");
+            }
+        }
+
+        [Test]
+        public void GetMainWindow_Should_Return_MainWindowAutomationElement()
+        {          
+            sut.WaitForInputIdle(500);
+
+            AutomationElement window = sut.GetMainWindow();
+
+            window.Current.NativeWindowHandle.Should().Be(sut.MainWindowHandle.ToInt32());
+        }
+
+        [Test]
+        public void GetMainWindow_Should_WaitForInputIdle()
+        {
+            AutomationElement window = sut.GetMainWindow();
+
+            window.Current.NativeWindowHandle.Should().NotBe(0);
+        }
+    }
+}
---
+++ UIAutomationClient.FluentExtensions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UIAutomationClient.FluentExtensions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("UIAutomationClient.FluentExtensions.Tests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d80eb58-0b24-4f5c-9d08-7bc51caac26a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
