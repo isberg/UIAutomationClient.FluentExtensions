42ad0909259aa8d46380f205597e5c3125787f08
Jonas Isberg <jonas.d.isberg@gmail.com>
2018-08-13 05:34:29 +0200

    feat: Condition factory (By)

---
+++ UIAutomationClient.FluentExtensions/By.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+namespace UIAutomationClient.FluentExtensions
+{
+    public static class By
+    {
+        public static Condition Name(string value)
+            => new PropertyCondition(AutomationElement.NameProperty, value);
+
+        public static Condition ClassName(string value)
+            => new PropertyCondition(AutomationElement.ClassNameProperty, value);
+
+        public static Condition AutomationId(string value)
+            => new PropertyCondition(AutomationElement.AutomationIdProperty, value);
+
+    }
+}
